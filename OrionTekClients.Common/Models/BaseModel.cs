7a9740de9f84aa3d3cda8415f7bcf46541e2d87e
Fernando Cruz <fernan2cruz@users.noreply.github.com>
2022-05-09 20:18:30 -0400

    Add project files.

---
+++ OrionTekClients.Common/Models/BaseModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrionTekClients.Common.Models
+{
+    /// <summary>
+    /// Clase Base de donde heredan todos los modelos
+    /// </summary>
+    public class BaseModel
+    {
+
+        public Guid Id { get; set; }
+
+        //Estos campos son de auditoria para los modelos
+        public string CreatedBy { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedDate { get;set; }
+    }
+}
---
+++ OrionTekClients.Common/Models/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrionTekClients.Common.Models
+{
+    public class Client : BaseModel
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime? BirthDay { get; set; }
+
+        
+    }
+}
